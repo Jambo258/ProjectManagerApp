75b7ddb727c6d9d781a5e1d0ab0ff6e4318b4725
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-01-02 14:17:07 +0200

    Add React Router and test navlinks for front page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { router } from "./routes/routes.tsx";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!)
+  .render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
