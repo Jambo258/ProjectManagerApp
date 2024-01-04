e5cc46238e53d1f578d336127b8c1d01b42f2727
TTyni <tommityni@msn.com>
2024-01-04 15:14:14 +0200

    create project modal

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,24 @@
 import { NavLink, Outlet } from "react-router-dom";
+import CreateProjectModal from "../features/createProject/CreateProjectModal";
 
 const App = () => {
-  return <>
-    <NavLink to="/" className="mr-5 text-dark-font text-4xl">Project Management App</NavLink>
-    <NavLink to="login" className="mr-5 hover:font-bold">Login</NavLink>
-    <NavLink to="register" className="hover:font-bold">Register</NavLink>
+  return (
+    <>
+      <NavLink to="/" className="mr-5 text-dark-font text-4xl">
+        Project Management App
+      </NavLink>
+      <NavLink to="login" className="mr-5 hover:font-bold">
+        Login
+      </NavLink>
+      <NavLink to="register" className="hover:font-bold">
+        Register
+      </NavLink>
 
-    <Outlet />
-  </>;
+      <Outlet />
+
+      <CreateProjectModal />
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/features/createProject/CreateProjectModal.tsx
@@ -0,0 +1,66 @@
+import Modal from "react-modal";
+import { useState } from "react";
+import projectServices from "./projectServices";
+
+Modal.setAppElement("#root");
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const CreateProjectModal = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [inputName, setInputName] = useState("default");
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  // const afterOpenModal = () => {
+  //   console.log("test");
+  // };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const newProject = async () => {
+    const content = [{}];
+    await projectServices
+      .createNewProject(inputName)
+      .then(() => projectServices.newPage("new page", 666, content));
+    closeModal();
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        //onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Create new project modal"
+      >
+        <input
+          type="text"
+          defaultValue={"Project name"}
+          required={true}
+          onChange={(e) => setInputName(e.target.value)}
+        />
+
+        <button onClick={() => newProject}>Create new project</button>
+        <button onClick={closeModal}>close</button>
+      </Modal>
+    </div>
+  );
+};
+
+export default CreateProjectModal;
