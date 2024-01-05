8b5ac4acafe73c2d24ea187e45ae7f2d0dcefab3
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-05 09:48:11 +0200

    Add LoginForm component into the loginView and refactor routes.tsx to use loginView on the /login route.

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -7,13 +7,14 @@ import App from "../app/App.tsx";
 import { HomeView } from "../pages/views/homeView.tsx";
 import { PublicPage } from "../pages/publicPage.tsx";
 import { RegisterView } from "../pages/views/registerView.tsx";
+import { LoginView } from "../pages/views/loginView.tsx";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<PublicPage />} >
         <Route index element={<HomeView/>} />
-        <Route path="login" element={<p className="text-dark-font text-2xl">Login</p>} />
+        <Route path="login" element={<LoginView />} />
         <Route path="register" element={<RegisterView />} />
       </Route>
     </Route>
