da97d580db57b191a1256e77b1ea9d0e134245b7
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-05 12:53:28 +0200

    registerForm: Refactor to use react-router's Form. Add form action.

--- frontend/src/features/auth/RegisterForm.tsx
+++ frontend/src/features/auth/RegisterForm.tsx
@@ -1,7 +1,31 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, Form, ActionFunctionArgs } from "react-router-dom";
+import { registerUserSchema } from "./authValidation";
 import { Eye, EyeOff } from "react-feather";
 import { useRegisterUserMutation } from "../api/apiSlice";
+
+const registerAction = async ({ request }: ActionFunctionArgs) => {
+  const data = await request.formData();
+
+  const submission = {
+    email: data.get("email"),
+    name: data.get("name"),
+    password: data.get("password")
+  };
+  console.log(submission);
+
+  // const isValid = await registerUserSchema.isValid(submission);
+  // const emailIsValid = await registerUserSchema.isValid(submission.email);
+  // const nameIsValid = await registerUserSchema.isValid(submission.name);
+  const isValid = await registerUserSchema.validate(submission);
+
+
+
+
+  console.log(isValid);
+
+  return null;
+};
 
 export const RegisterForm = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -36,56 +60,69 @@ export const RegisterForm = () => {
   };
 
   return (
-    <section className="my-8 w-fit mx-auto">
-      <h2 className="font-sans heading-xl text-dark-font uppercase leading-none w-fit mx-auto mb-6">Create <br /> your account</h2>
-      <form
-        onSubmit={handleSubmit}
-        className="mx-auto relative">
+    <section className="w-fit mt-14 mx-auto">
+      <h2 className="font-sans heading-xl text-dark-font uppercase leading-none w-fit mx-auto mb-6">
+        Create <br /> your account
+      </h2>
+      <Form
+        method="post"
+        action="/register">
 
         <label
-          htmlFor="emailInput"
-          className="body-text-sm text-dark-font block mb-1">Email:</label>
-        <input
-          id="emailInput"
-          type="text"
-          value={email}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-          placeholder="john.doe@mail.com"
-          required
-          className="body-text-md py-1.5 px-4 mb-3 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50" />
+          className="body-text-sm text-dark-font block mb-1">
+            Email:
+          <input
+            name="email"
+            type="text"
+            placeholder="john.doe@mail.com"
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+            required
+            className="body-text-md py-1.5 px-4 mb-3 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50" />
+        </label>
 
         <label
-          htmlFor="nameInput"
-          className="body-text-sm text-dark-font block mb-1">Full name:</label>
-        <input
-          id="nameInput"
-          type="text"
-          value={name}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-          placeholder="John Doe"
-          required
-          className="body-text-md py-1.5 px-4 mb-3 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50" />
+          className="body-text-sm text-dark-font block mb-1">
+            Full name:
+          <input
+            name="name"
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+            required
+            className="body-text-md py-1.5 px-4 mb-3 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50"/>
+        </label>
 
         <label
-          htmlFor="passwordInput"
-          className="body-text-sm text-dark-font block mb-1">Password:</label>
-        <input
-          id="passwordInput"
-          type={isVisible ? "text" : "password"}
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-          required
-          className="body-text-md py-1.5 px-4 mb-8 w-full inline-block focus:outline-none focus:ring focus:ring-dark-blue-50" />
-        <button
-          onClick={() => setIsVisible(!isVisible)}
-          className="bg-grayscale-0 px-2 py-2.5 rounded-l-none absolute right-0 align-middle focus:outline-none focus:ring focus:ring-dark-blue-50">
-          {isVisible ? <Eye size={18} /> : <EyeOff size={18} />}
-        </button>
+          className="body-text-sm text-dark-font block mb-1">
+            Password:
+          <section className="mx-auto relative">
+            <input
+              name="password"
+              type={isVisible ? "text" : "password"}
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              required
+              className="body-text-md py-1.5 px-4 mb-8 w-full inline-block focus:outline-none focus:ring focus:ring-dark-blue-50"/>
+            <button
+              onClick={() => setIsVisible(!isVisible)}
+              className="bg-grayscale-0 px-2 py-2.5 rounded-l-none absolute right-0 align-middle focus:outline-none focus:ring focus:ring-dark-blue-50">
+              {isVisible ? <Eye size={18}/> : <EyeOff size={18}/>}
+            </button>
+          </section>
+        </label>
 
         <button type="submit" className="w-full btn-text-md focus:outline-none focus:ring focus:ring-dark-blue-50">Register</button>
-      </form>
+      </Form>
+
       <p className="body-text-sm text-dark-font mt-3 mb-1 text-center">Already have an account?</p>
-      <Link to="/login" className="focus:outline-dark-blue-50"><p className="body-text-md text-dark-font underline text-center">Login</p></Link>
+      <Link to="/login" className="focus:outline-dark-blue-50">
+        <p className="body-text-md text-dark-font underline text-center">Login</p>
+      </Link>
+
     </section>
   );
 };
+
+RegisterForm.action = registerAction;
