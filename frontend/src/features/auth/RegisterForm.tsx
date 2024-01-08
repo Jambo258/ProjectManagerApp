430f721b89a87628c50fa1fa1c448d9d9855b2ac
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-08 13:57:02 +0200

    RegisterForm: refactor component to use react hook form

--- frontend/src/features/auth/RegisterForm.tsx
+++ frontend/src/features/auth/RegisterForm.tsx
@@ -1,68 +1,71 @@
-import React, {useState} from "react";
-import { Link } from "react-router-dom";
-// import { registerUserSchema } from "./authValidation";
-import { Eye, EyeOff } from "react-feather";
+import {useState} from "react";
+
+// Redux Toolkit
 import { useRegisterUserMutation } from "../api/apiSlice";
 
-// const registerAction = async ({ request }: ActionFunctionArgs) => {
-//   const data = await request.formData();
+// React Router
+import { Link } from "react-router-dom";
 
-//   const submission = {
-//     email: data.get("email"),
-//     name: data.get("name"),
-//     password: data.get("password")
-//   };
-//   console.log(submission);
+// Hook Form and Yup
+import { useForm } from "react-hook-form";
+import { registerUserSchema } from "./authValidation";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { DevTool } from "@hookform/devtools";
 
-//   // const isValid = await registerUserSchema.isValid(submission);
-//   // const emailIsValid = await registerUserSchema.isValid(submission.email);
-//   // const nameIsValid = await registerUserSchema.isValid(submission.name);
-//   const isValid = await registerUserSchema.validate(submission);
+// Components
+import { Eye, EyeOff } from "react-feather";
 
-//   console.log(isValid);
-
-//   return null;
-// };
+// Interfaces
+interface FormValues {
+  email: string;
+  name: string;
+  password: string;
+}
 
 export const RegisterForm = () => {
   const [registerUser, { isLoading }] = useRegisterUserMutation();
-  const [isVisible, setIsVisible] = useState(false);
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [emailError, setEmailError] = useState<null | string>("null");
-  const [nameError, setNameError] = useState<null | string>(null);
-  const [passwordError, setPasswordError] = useState<null | string>(null);
-
-
-  // Validate inputs
-
-
-  const canSave = [email, name, password].every(Boolean) && !isLoading;
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Email:", email);
-    console.log("Name:", name);
-    console.log("Password:", password);
-
-    if (canSave) {
-      try {
-        const user = await registerUser({ email, name, password }).unwrap();
-        console.log("user:", user);
-        setEmail("");
-        setName("");
-        setPassword("");
-      } catch (err) {
-        console.error("Failed to save the user: ", err);
-      }
+  const { control, formState: {errors}, handleSubmit, register } = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
     }
-    // To do
-    // input validation
-    // error handling / message
-    // if registration is successfull, log the user in and redirect user to the "user's homepage"
+  });
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onHandleSubmit = (formData: FormValues) => {
+    console.log("Register form submitted", formData);
   };
+
+  // const [email, setEmail] = useState("");
+  // const [name, setName] = useState("");
+  // const [password, setPassword] = useState("");
+
+
+  // const canSave = [email, name, password].every(Boolean) && !isLoading;
+
+  // const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   console.log("Email:", email);
+  //   console.log("Name:", name);
+  //   console.log("Password:", password);
+
+  //   if (canSave) {
+  //     try {
+  //       const user = await registerUser({ email, name, password }).unwrap();
+  //       console.log("user:", user);
+  //       setEmail("");
+  //       setName("");
+  //       setPassword("");
+  //     } catch (err) {
+  //       console.error("Failed to save the user: ", err);
+  //     }
+  //   }
+  //   // To do
+  //   // input validation
+  //   // error handling / message
+  //   // if registration is successfull, log the user in and redirect user to the "user's homepage"
+  // };
 
   return (
     <section className="w-fit mt-14 mx-auto">
@@ -70,34 +73,28 @@ export const RegisterForm = () => {
         Create <br /> your account
       </h2>
       <form
-        onSubmit={handleSubmit}>
+        onSubmit={handleSubmit(onHandleSubmit)} noValidate>
 
         <label
           className="body-text-sm text-dark-font block mb-3">
             Email:
           <input
-            name="email"
             type="text"
+            {...register("email")}
             placeholder="john.doe@mail.com"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-            required
             className="body-text-md py-1.5 px-4 mt-1 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50" />
-          {emailError ? <p className="text-center body-text-xs text-caution-200">{emailError}</p> : <></>}
+          <p className="text-center body-text-xs text-caution-200">{errors.email?.message}</p>
         </label>
 
         <label
           className="body-text-sm text-dark-font block mb-3">
             Full name:
           <input
-            name="name"
             type="text"
+            {...register("name")}
             placeholder="John Doe"
-            value={name}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-            required
             className="body-text-md py-1.5 px-4 mt-1 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50"/>
-          {nameError ? <p className="text-center body-text-xs text-caution-200">{nameError}</p> : <></>}
+          <p className="text-center body-text-xs text-caution-200">{errors.name?.message}</p>
         </label>
 
         <label
@@ -105,11 +102,8 @@ export const RegisterForm = () => {
             Password:
           <section className="mx-auto mt-1 relative">
             <input
-              name="password"
               type={isVisible ? "text" : "password"}
-              value={password}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-              required
+              {...register("password")}
               className="body-text-md py-1.5 px-4 w-full inline-block focus:outline-none focus:ring focus:ring-dark-blue-50"/>
             <button
               onClick={() => setIsVisible(!isVisible)}
@@ -117,11 +111,14 @@ export const RegisterForm = () => {
               {isVisible ? <Eye size={18}/> : <EyeOff size={18}/>}
             </button>
           </section>
-          {passwordError ? <p className="text-center body-text-xs text-caution-200">{passwordError}</p> : <></>}
+          <p className="text-center body-text-xs text-caution-200">{errors.password?.message}</p>
         </label>
 
         <button type="submit" className="w-full btn-text-md focus:outline-none focus:ring focus:ring-dark-blue-50">Register</button>
       </form>
+
+      {/* For development only */}
+      <DevTool control={control}/>
 
       <p className="body-text-sm text-dark-font mt-3 mb-1 text-center">Already have an account?</p>
       <Link to="/login" className="focus:outline-dark-blue-50">
