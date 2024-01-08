43790c360387320ac1ad65f1a16d7a6f9518488e
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-08 13:55:33 +0200

    authValidation: add error messages for the required errors

--- frontend/src/features/auth/authValidation.ts
+++ frontend/src/features/auth/authValidation.ts
@@ -1,9 +1,9 @@
 import * as yup from "yup";
 
-export const registerUserSchema = yup.object({
-  email: yup.string().email("Must be valid email").required(),
-  name: yup.string().trim().min(2, "Must be at least 2 characters long").max(50).required(),
-  password: yup.string().min(6, "Must be at least 6 characters long").required()
+export const registerUserSchema = yup.object().shape({
+  email: yup.string().email("Must be a valid email").required("Email is required"),
+  name: yup.string().trim().min(2, "Must be at least 2 characters long").max(50, "Must be less than 50 characters long").required("Full name is required"),
+  password: yup.string().min(6, "Must be at least 6 characters long").required("Password is required")
 });
 
 export type registerUserSchemaType = yup.InferType<typeof registerUserSchema>;
