8d655e872d3597f7a1279c6a6015fa957f1769f1
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-09 09:57:48 +0200

    routes: merge fix conflict

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -8,10 +8,7 @@ import { HomeView } from "../pages/views/homeView.tsx";
 import { PublicPage } from "../pages/publicPage.tsx";
 import { RegisterView } from "../pages/views/registerView.tsx";
 import { LoginView } from "../pages/views/loginView.tsx";
-<<<<<<< HEAD
 import { ProfileModal } from "../pages/components/profilemodal.tsx";
-=======
->>>>>>> f0c31f0 (routes: remove action from the /register route)
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,12 +16,8 @@ export const router = createBrowserRouter(
       <Route path="/" element={<PublicPage />} >
         <Route index element={<HomeView/>} />
         <Route path="login" element={<LoginView />} />
-<<<<<<< HEAD
         <Route path="profile" element={<ProfileModal/>} />
         <Route path="register" element={<RegisterView />} />
-=======
-        <Route path="register" element={<RegisterView />}/>
->>>>>>> f0c31f0 (routes: remove action from the /register route)
       </Route>
     </Route>
   )
