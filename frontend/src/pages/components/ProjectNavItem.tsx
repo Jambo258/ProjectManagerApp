458cf4eee657b740be8ffb63b185614bde41f647
TTyni <tommityni@msn.com>
2024-01-11 15:47:18 +0200

    end of day commit

--- frontend/src/pages/components/ProjectNavItem.tsx
+++ frontend/src/pages/components/ProjectNavItem.tsx
@@ -4,55 +4,63 @@ import { Link } from "react-router-dom";
 
 // Interfaces for testing purposes
 interface Page {
-  id: number,
-  name: string
+  id: number;
+  name: string;
+}
+
+interface User {
+  role: string;
+  userid: number;
 }
 
 interface Project {
-  id: number,
-  name: string,
-  pages: Page[]
+  id: number;
+  name: string;
+  pages: Page[];
+  users: User[];
+  created_at: string;
+  updated_at: string;
 }
 
 interface ProjectNavItemProps {
-  project: Project
+  project: Project;
 }
 
-export const ProjectNavItem = ({project}: ProjectNavItemProps) => {
+export const ProjectNavItem = ({ project }: ProjectNavItemProps) => {
   const [showPages, setShowPages] = useState<boolean>(false);
 
   return (
     <section>
-      <div
-        className="bg-dark-blue-200 border-b border-solid border-dark-blue-100 px-6 py-3 overflow-auto flex justify-between">
-
+      <div className="bg-dark-blue-200 border-b border-solid border-dark-blue-100 px-6 py-3 overflow-auto flex justify-between">
         <Link to={"/"}>
-          <button onClick={() => console.log("Open " + project.name)} className="leading-8 heading-xs m-0 p-0 text-left bg-grayscale-0 hover:bg-grayscale-0 text-light-font">
+          <button
+            onClick={() => console.log("Open " + project.name)}
+            className="leading-8 heading-xs m-0 p-0 text-left bg-grayscale-0 hover:bg-grayscale-0 text-light-font"
+          >
             {project.name}
           </button>
         </Link>
 
-        <button className="font-light heading-xs m-0 p-0 bg-grayscale-0 hover:bg-grayscale-0 text-light-font hover:text-primary-200"
-          onClick={() => setShowPages(!showPages)}>
+        <button
+          className="font-light heading-xs m-0 p-0 bg-grayscale-0 hover:bg-grayscale-0 text-light-font hover:text-primary-200"
+          onClick={() => setShowPages(!showPages)}
+        >
           {showPages ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
         </button>
-
       </div>
 
-      {showPages ? (
-        project.pages.map(page => (
+      {showPages &&
+        project.pages.map((page) => (
           <Link key={page.id} to={"/"}>
-            <button 
+            <button
               onClick={() => console.log("Open " + page.name)}
-              className="border-b border-solid border-dark-blue-100 body-text-sm px-6 py-3 
-              hover:bg-dark-blue-100  w-full text-left bg-grayscale-0 text-light-font">
+              className="border-b border-solid border-dark-blue-100 body-text-sm px-6 py-3
+              hover:bg-dark-blue-100  w-full text-left bg-grayscale-0 text-light-font"
+            >
               {page.name}
             </button>
           </Link>
-        ))
-      )
-        : null}
+        ))}
     </section>
-
   );
 };
