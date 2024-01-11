cd917fc677391eb79d5f1048abd130a9d63be99f
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-11 11:25:23 +0200

    DasboardNav: fix to match code review suggestions

--- frontend/src/pages/components/DashboardNav.tsx
+++ frontend/src/pages/components/DashboardNav.tsx
@@ -53,50 +53,48 @@ export const DashboardNav = () => {
     <nav className={`bg-dark-blue-300 min-h-screen text-light-font ${collapseNav ? "w-72" : "w-12"}`}>
       <div className="min-h-[calc(100vh-4rem)] h-[calc(100%-4rem)]">
         <div className="grid grid-flow-col justify-end">
-          <button className="w-fit text-light-font hover:text-primary-200 bg-grayscale-0 hover:bg-grayscale-0 p-4 heading-md"
+          <button className="w-fit p-4 heading-md text-light-font hover:text-primary-200 bg-grayscale-0 hover:bg-grayscale-0"
             onClick={() => setcollapseNav(!collapseNav)}>
             {collapseNav ? <ChevronLeft size={20} /> : <ChevronRight size={20} />}
           </button>
         </div>
 
-        {collapseNav
-          ? <section>
-            <h4 className="px-6 mb-6 heading-md">
+        {collapseNav && <section>
+          <h4 className="px-6 mb-6 heading-md">
               Project <br /> Management App
-            </h4>
+          </h4>
 
-            <div className="grid grid-flow-col items-center px-6 py-4 border-b border-solid border-dark-blue-100">
-              <div className="heading-sm">
+          <div className="grid grid-flow-col px-6 py-4 items-center border-b border-solid border-dark-blue-100">
+            <div className="heading-sm">
                 My projects
-              </div>
-              <div className="text-right">
-                <CreateProjectModal />
-              </div>
             </div>
+            <div className="text-right">
+              <CreateProjectModal />
+            </div>
+          </div>
 
-            {exampleProjects.map(project =>
-              <div key={project.id}>
-                <ProjectNavItem project={project} />
-              </div>
-            )}
-          </section>
-
-          : null}
+          {exampleProjects.map(project =>
+            <div key={project.id}>
+              <ProjectNavItem project={project} />
+            </div>
+          )}
+        </section>}
       </div>
 
-      {collapseNav
-        ? <section className="px-4 py-2 grid grid-flow-col items-center bg-dark-blue-100 w-full h-16">
-          <ProfileModal />
+      {collapseNav &&
+      <section className="grid grid-flow-col w-full h-16 px-4 py-2 items-center bg-dark-blue-100">
+        <ProfileModal />
 
-          <div>
-            <button className="bg-grayscale-0 heading-xs text-light-font hover:text-primary-200 w-fit p-2 float-right
-              hover:bg-grayscale-0 " onClick={() => console.log("Log out")}>
-              <p>Log out</p>
-            </button>
-          </div>
-        </section>
-
-        : null}
+        <div>
+          <button
+            onClick={() => console.log("Log out")}
+            className="float-right w-fit p-2 heading-xs text-light-font bg-grayscale-0 hover:text-primary-200 hover:bg-grayscale-0"
+          >
+            <p>Log out</p>
+          </button>
+        </div>
+      </section>
+      }
 
     </nav>
   );
