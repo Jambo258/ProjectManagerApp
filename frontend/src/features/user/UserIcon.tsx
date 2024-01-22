83ee597faf134441d132ea8bee7b86020084e4b7
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-01-22 14:35:19 +0200

    Change text colors to show better from different backgrounds

--- frontend/src/features/user/UserIcon.tsx
+++ frontend/src/features/user/UserIcon.tsx
@@ -9,21 +9,21 @@ interface Color {
 }
 
 const colors: Color[] = [
-  {bg: "bg-red-200", text: "text-light-font"},
-  {bg: "bg-purple-200", text: "text-light-font"},
-  {bg: "bg-blue-200", text: "text-dark-font"},
-  {bg: "bg-green-200", text: "text-dark-font"},
-  {bg: "bg-yellow-200", text: "text-dark-font"},
-  {bg: "bg-red-300", text: "text-light-font"},
-  {bg: "bg-purple-300", text: "text-light-font"},
-  {bg: "bg-blue-300", text: "text-light-font"},
-  {bg: "bg-green-300", text: "text-light-font"},
+  {bg: "bg-red-200", text: "text-grayscale-100"},
+  {bg: "bg-purple-200", text: "text-grayscale-100"},
+  {bg: "bg-blue-200", text: "font-grayscale-100"},
+  {bg: "bg-success-100", text: "text-dark-font"},
+  {bg: "bg-caution-100", text: "text-dark-font"},
+  {bg: "bg-red-300", text: "text-grayscale-100"},
+  {bg: "bg-purple-300", text: "text-grayscale-100"},
+  {bg: "bg-blue-300", text: "text-grayscale-100"},
+  {bg: "bg-green-300", text: "text-dark-font"},
   {bg: "bg-yellow-300", text: "text-dark-font"}
 ];
 
 export const UserIcon = ({id, name}: UserIconProps) => {
   return (
-    <div className={"rounded-full m-0 p-0 w-8 h-8 " + colors[id % 10].text + " text-center heading-sm leading-8 " + colors[id % 10].bg + " cursor-default"}>
+    <div className={"rounded-full m-0 p-0 w-8 h-8 " + colors[id % 10].text + " text-center heading-xs leading-8 " + colors[id % 10].bg + " cursor-default"}>
       {name[0].toUpperCase()}
     </div>  
   );
