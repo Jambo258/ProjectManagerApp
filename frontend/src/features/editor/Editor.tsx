e5044490a438c2550a70bf01bc94ed097fa81c24
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-22 15:55:53 +0200

    Editor: change border radius to match other components

--- frontend/src/features/editor/Editor.tsx
+++ frontend/src/features/editor/Editor.tsx
@@ -71,7 +71,7 @@ const Editor = ({ pageId }: IProps) => {
     ],
     editorProps: {
       attributes: {
-        class: "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl p-2 focus:outline-none min-h-[10rem]",
+        class: "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl p-2 focus:outline-none border border-grayscale-400 rounded-b min-h-[10rem]",
       },
     },
   });
