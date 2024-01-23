c168517ae74db9b0ab40eac7931400f6a740987c
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-01-23 10:00:08 +0200

    Add indicators of active project and page for DashboardNav

--- frontend/src/features/user/ProjectNavItem.tsx
+++ frontend/src/features/user/ProjectNavItem.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ChevronDown, ChevronUp } from "react-feather";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import type { Project } from "../../features/api/apiSlice";
 
 interface ProjectNavItemProps {
@@ -13,13 +13,16 @@ export const ProjectNavItem = ({ project }: ProjectNavItemProps) => {
   return (
     <section>
       <div className="bg-dark-blue-200 border-b border-solid border-dark-blue-100 px-6 py-3 overflow-auto flex justify-between">
-        <Link
+        <NavLink
           to={`/projects/${project.id}`}
           onClick={() => console.log("Open " + project.name)}
-          className="m-0 p-0 text-left leading-8 heading-xs text-light-font bg-grayscale-0 hover:bg-grayscale-0 focus:outline-none focus:ring-0 focus:text-caution-100"
-        >
+          className={({ isActive }) =>
+            isActive
+              ? "m-0 p-0 text-left leading-8 heading-xs text-caution-100 bg-grayscale-0 hover:bg-grayscale-0 focus:outline-none focus:ring-0 focus:text-caution-100"
+              : "m-0 p-0 text-left leading-8 heading-xs text-light-font bg-grayscale-0 hover:bg-grayscale-0 focus:outline-none focus:ring-0 focus:text-caution-100"
+          }>
           {project.name}
-        </Link>
+        </NavLink>
 
         <button
           className="m-0 p-0 font-light heading-xs text-light-font hover:text-primary-200 bg-grayscale-0 hover:bg-grayscale-0 focus:outline-none focus:ring-0 focus:text-caution-100"
@@ -33,14 +36,18 @@ export const ProjectNavItem = ({ project }: ProjectNavItemProps) => {
 
       {showPages &&
         project.pages.map((page) => (
-          <Link
+          <NavLink
             key={page.id}
             to={`/projects/${project.id}/${page.id}`}
             onClick={() => console.log("Open " + page.name)}
-            className="block w-full px-6 py-3 text-left body-text-sm text-light-font border-b border-solid border-dark-blue-100 hover:bg-dark-blue-100 bg-grayscale-0 focus:outline-none focus:bg-dark-blue-100"
+            className={({ isActive }) =>
+              isActive 
+                ? "block w-full px-6 py-3 text-left body-text-sm text-light-font underline border-b border-solid border-dark-blue-100 hover:bg-dark-blue-100 bg-grayscale-0 focus:outline-none focus:bg-dark-blue-100"
+                : "block w-full px-6 py-3 text-left body-text-sm text-light-font border-b border-solid border-dark-blue-100 hover:bg-dark-blue-100 bg-grayscale-0 focus:outline-none focus:bg-dark-blue-100"
+            }
           >
             {page.name}
-          </Link>
+          </NavLink>
         ))}
     </section>
   );
