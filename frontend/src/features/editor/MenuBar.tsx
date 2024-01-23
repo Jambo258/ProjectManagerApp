48c099faca8e79cb3837e4c68d5e17f771fecbdf
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-23 13:54:33 +0200

    MenuBar: change background color to white

--- frontend/src/features/editor/MenuBar.tsx
+++ frontend/src/features/editor/MenuBar.tsx
@@ -133,7 +133,7 @@ const MenuBar = ({ editor }: { editor: Editor; }) => {
   ];
 
   return (
-    <div className="buttons flex items-center flex-wrap gap-x-2 border-grayscale-400 rounded-t border-t border-x p-2 mx-5 mt-5">
+    <div className="flex items-center flex-wrap gap-x-2 bg-grayscale-100 border-grayscale-400 rounded-t border-t border-x p-2 mx-5 mt-5">
       {items.map((item, index) => (
         <Fragment key={index}>
           {item.type === "divider" ? <div className="w-px h-8 bg-grayscale-300" /> : <MenuItem {...item} />}
