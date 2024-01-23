9cedf26f78a82c6941753ae7f1db749a4484cf0f
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-23 12:30:15 +0200

    Editor: add tiptap to the classes in the editorProps's attributes to enable the use of tailwind in styling

--- frontend/src/features/editor/Editor.tsx
+++ frontend/src/features/editor/Editor.tsx
@@ -71,7 +71,7 @@ const Editor = ({ pageId }: IProps) => {
     ],
     editorProps: {
       attributes: {
-        class: "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl p-2 focus:outline-none border border-grayscale-400 rounded-b min-h-[10rem]",
+        class: "tiptap prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl mx-5 p-2 focus:outline-none border border-grayscale-400 rounded-b min-h-[10rem]",
       },
     },
   });
