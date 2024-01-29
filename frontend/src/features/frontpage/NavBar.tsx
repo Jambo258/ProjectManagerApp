aba2ee33204583a7c046784448ec84d20570a846
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-01-29 12:45:44 +0200

    Update PublicPage scalability

--- frontend/src/features/frontpage/NavBar.tsx
+++ frontend/src/features/frontpage/NavBar.tsx
@@ -1,28 +1,55 @@
+import { useEffect, useState } from "react";
+import { Menu } from "react-feather";
 import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
+  const [width, setWidth]  = useState(window.innerWidth);
+  const [openMenu, setOpenMenu] = useState<boolean>(width > 640 ? true : false);
+  
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+  
+  useEffect(() => {
+    setOpenMenu(false);
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+
   return (
-    <div className="w-full h-fit bg-dark-blue-300 grid grid-flow-col items-center px-6 py-4 drop-shadow-md">
-      <div className="heading-lg text-light-font leading-tight outline-none focus:outline focus:outline-primary-200">
-        <NavLink to="/">
+    <header className="grid grid-flow-row md:grid-flow-col min-h-24 w-full-fit md:bg-dark-blue-300 bg-dark-blue-100 drop-shadow-md">
+      <section className="grid grid-flow-col w-full h-24 items-center px-6 py-4 bg-dark-blue-300">
+        <h1 
+          className="heading-sm sm:heading-md md:heading-  text-light-font leading-tight text-left outline-none focus:outline focus:outline-primary-200"
+          onClick={() => setOpenMenu(false)}>
+          <NavLink to="/">
           Project <br />
           Management App
+          </NavLink>
+        </h1>
+
+        <section className="flex w-full align-middle justify-end h-full md:hidden">
+          <button className="w-fit bg-grayscale-0 p-2 m-0 hover:bg-grayscale-0 text-light-font" onClick={() => setOpenMenu(!openMenu)}>
+            <Menu size={28} />
+          </button>
+        </section>
+      </section>     
+
+      {(width > 768 || openMenu) &&
+      <nav className="grid grid-flow-col content-center items-center justify-end text-right gap-4  p-4">
+        <NavLink to="register">
+          <button className="btn-text-xs w-full px-4 py-1.5 outline-none focus:outline focus:outline-primary-200" onClick={() => setOpenMenu(false)}>
+            Register
+          </button>
         </NavLink>
-      </div>
-      <div className="text-right pr-6">
-        <>
-          <NavLink to="register">
-            <button className="btn-text-xs px-4 py-1.5 mr-2 outline-none focus:outline focus:outline-primary-200">
-              Register
-            </button>
-          </NavLink>
-          <NavLink to="login">
-            <button className="btn-text-xs px-4 py-1.5 outline-none focus:outline focus:outline-primary-200">
-              Login
-            </button>
-          </NavLink>
-        </>
-      </div>
-    </div>
+        <NavLink to="login">
+          <button className="btn-text-xs w-full px-4 py-1.5 outline-none focus:outline focus:outline-primary-200" onClick={() => setOpenMenu(false)}>
+            Login
+          </button>
+        </NavLink>
+      </nav>
+      }
+    </header>
   );
 };
