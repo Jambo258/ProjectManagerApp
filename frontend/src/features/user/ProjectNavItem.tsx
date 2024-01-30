52a4863324455f768d33aba3619457172845a0cb
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-01-30 11:19:51 +0200

    Check if names include spaces in ProjectNavItem

--- frontend/src/features/user/ProjectNavItem.tsx
+++ frontend/src/features/user/ProjectNavItem.tsx
@@ -11,13 +11,21 @@ interface ProjectNavItemProps {
 export const ProjectNavItem = ({ project, closeNav }: ProjectNavItemProps) => {
   const [showPages, setShowPages] = useState<boolean>(false);
 
+  const hasSpaces = (name: string) => {
+    if (name.includes(" ")) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
     <section>
       <div className="flex bg-dark-blue-200 border-b border-solid border-dark-blue-100 px-6 py-3 overflow-auto justify-between">
         <Link
           to={`/projects/${project.id}`}
           onClick={() => closeNav()}
-          className={`overflow-x-hidden *:m-0 p-0 text-left leading-8 heading-xs bg-grayscale-0 hover:bg-grayscale-0 focus:outline-none focus:ring-0 focus:text-caution-100 ${window.location.pathname.includes(`/projects/${project.id}`) ? "text-caution-100" : "text-light-font"}`} >
+          className={`overflow-x-hidden *:m-0 p-0 text-left leading-8 heading-xs bg-grayscale-0 hover:bg-grayscale-0 focus:outline-none focus:ring-0 focus:text-caution-100 ${window.location.pathname.includes(`/projects/${project.id}`) ? "text-caution-100" : "text-light-font"} ${!hasSpaces(project.name) && "break-all"}`} >
           {project.name}
         </Link>
 
@@ -37,7 +45,7 @@ export const ProjectNavItem = ({ project, closeNav }: ProjectNavItemProps) => {
             key={page.id}
             to={`/projects/${project.id}/${page.id}`}
             onClick={() => closeNav()}
-            className={`block overflow-x-hidden w-full px-6 py-3 text-left body-text-sm text-light-fonts border-b border-solid border-dark-blue-100 hover:bg-dark-blue-100 bg-grayscale-0 focus:outline-none focus:bg-dark-blue-100 ${window.location.pathname.includes(`/projects/${project.id}/${page.id}`) && "underline"}`}
+            className={`block overflow-x-hidden w-full px-6 py-3 text-left body-text-sm text-light-fonts border-b border-solid border-dark-blue-100 hover:bg-dark-blue-100 bg-grayscale-0 focus:outline-none focus:bg-dark-blue-100 ${window.location.pathname.includes(`/projects/${project.id}/${page.id}`) && "underline"} ${!hasSpaces(page.name) && "break-all"}`}
           >
             {page.name}
           </Link>
