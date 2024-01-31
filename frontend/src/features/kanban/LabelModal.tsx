c7286395f4c2de56444a89b27f31640440dc117e
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-31 09:28:05 +0200

    LabelModal: fix to work with the changes in SubModal

--- frontend/src/features/kanban/LabelModal.tsx
+++ frontend/src/features/kanban/LabelModal.tsx
@@ -1,7 +1,6 @@
 import { type Labels } from "./Kanban";
 import { CreateLabelModal } from "./CreateLabelModal";
 // import { Modal } from "../../components/Modal";
-import { Edit2 } from "react-feather";
 import { Square } from "react-feather";
 import { EditLabelModal } from "./EditLabelModal";
 import { SubModal } from "./SubModal";
@@ -34,8 +33,8 @@ export const LabelModal = ({ label, setLabel, labels, setIsModalsOpen, isModalsO
             </div>
 
             <SubModal
-              btnText={<Edit2 size={24} />}
-              btnStyling={"bg-grayscale-0 hover:bg-grayscale-0"}
+              btnText="Edit label"
+              iconName="none"
               modalTitle={"Edit Label"}
               setIsModalsOpen={setIsModalsOpen}
               isModalsOpen={isModalsOpen}
@@ -52,8 +51,8 @@ export const LabelModal = ({ label, setLabel, labels, setIsModalsOpen, isModalsO
       </div>
       <section className="grid grid-cols mt-4">
         <SubModal
+          iconName="none"
           btnText={"Create new Label"}
-          btnStyling={"mb-3 mx-2"}
           modalTitle={"Create Label"}
           setIsModalsOpen={setIsModalsOpen}
           isModalsOpen={isModalsOpen}
