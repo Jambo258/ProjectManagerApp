47b37eea2fc688663c4a1850f264137f28b81e49
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-02-02 13:49:06 +0200

    Add icons for components

--- frontend/src/features/page/AddComponentModal.tsx
+++ frontend/src/features/page/AddComponentModal.tsx
@@ -1,11 +1,13 @@
-import { useContext } from "react";
+import { ReactElement, useContext } from "react";
 import { ModalContext } from "../../components/Modal";
+import { Columns, FileText, Calendar } from "react-feather";
 
 interface Component {
   name: string,
   type: "editor" | "kanban" | "calendar";
   description: string,
-  btnTxt: string
+  btnTxt: string,
+  icon: ReactElement
 }
 
 interface AddComponentModalProps {
@@ -17,19 +19,22 @@ const components: Component[] = [
     name: "Kanban board",
     type: "kanban",
     description: "Visualize and manage project wih an interactive Kanban board.",
-    btnTxt: "Add kanban board"
+    btnTxt: "Add kanban board",
+    icon: <Columns size={20}/>
   },
   {
     name: "Text editor",
     type: "editor",
     description: "Create and edit content effortlessly with an interactive text editor.",
-    btnTxt: "Add text editor"
+    btnTxt: "Add text editor",
+    icon: <FileText size={20}/>
   },
   {
     name: "Calendar",
     type: "calendar",
     description: "Stay organized and plan your meetings effectively with a calendar.",
-    btnTxt: "Add calendar"
+    btnTxt: "Add calendar",
+    icon: <Calendar size={20}/>
   }
 ];
 
@@ -44,9 +49,8 @@ export const AddComponentModal = ({addComponent}: AddComponentModalProps) => {
   const componentItem = (index: number, component: Component) => {
     return (
       <section key={index} className="w-[48%] sm:w-[31%] max-w-[300px] flex flex-col gap-2 items-stretch text-left mb-4">
-        <div className="w-full aspect-[4/3] bg-grayscale-300" />
-        <h4 className="heading-xs px-1">
-          {component.name}
+        <h4 className="heading-xs px-1 flex flex-row gap-2 items-center">
+          {component.icon} {component.name}
         </h4>
         <p className="body-text-sm mb-2 px-1 min-h-16">
           {component.description}
