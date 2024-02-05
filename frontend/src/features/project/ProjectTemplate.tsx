49ae162879a9f5edac9b6d0b658bfd31a43df847
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-02-05 11:05:41 +0200

    Fix Create Project routing and

--- frontend/src/features/project/ProjectTemplate.tsx
+++ frontend/src/features/project/ProjectTemplate.tsx
@@ -1,18 +1,15 @@
 import { useParams } from "react-router-dom";
-import { useAppSelector } from "../../app/hooks";
 import { useGetProjectQuery } from "../api/apiSlice";
 
 import { Modal } from "../../components/Modal";
 import { AddPageModal } from "../page/AddPageModal";
 
 export const ProjectTemplate = () => {
-  const user = useAppSelector((state) => state.auth.user);
-  
   const projectId = parseInt(useParams().projectId!);
   const project = useGetProjectQuery(projectId);
 
-  const handleView = () => {
-    if (user && project.data && project.data.pages.length > 0) {
+  const handleText = () => {
+    if (project.data && project.data.pages.length > 0) {
       return "Add new page or check one of the existing ones!";
     } else {
       return "It seems like you don't have any pages yet!";
@@ -21,7 +18,7 @@ export const ProjectTemplate = () => {
 
   return (
     <section className="p-4 sm:p-8 max-h-full h-screen max-w-full overflow-auto flex flex-col gap-6 justify-center">
-      <p className="mx-auto">{handleView()}</p> 
+      <p className="mx-auto">{handleText()}</p> 
       
       <Modal
         btnText={"Add new page"}
--- frontend/src/features/project/ProjectView.tsx
+++ frontend/src/features/project/ProjectView.tsx
@@ -1,25 +1,21 @@
-import { useAppSelector } from "../../app/hooks";
 import { Modal } from "../../components/Modal";
 import { useGetProjectsQuery } from "../api/apiSlice";
 import CreateProjectModal from "./CreateProjectModal";
 
 export const ProjectView = () => {
-  const user = useAppSelector((state) => state.auth.user);
-  const projects = useGetProjectsQuery(user.id);
+  const { data: projects = [] } = useGetProjectsQuery();
   
-  console.log(projects.data);
-  
-  const handleView = () => {
-    if (user && projects.data?.[0]) {
-      return "Create new project or check one of the existing ones!";
-    } else {
+  const handleText = () => {
+    if (projects.length === 0) {
       return "It seems like you don't have any projects yet!";
+    } else {
+      return "Create new project or check one of the existing ones!";
     }
   };
 
   return (
     <section className="p-4 sm:p-8 max-h-full h-screen max-w-full overflow-auto flex flex-col gap-6 justify-center">
-      <p className="mx-auto">{handleView()}</p>
+      <p className="mx-auto">{handleText()}</p>
             
       <Modal
         btnText={"Create new project"}
