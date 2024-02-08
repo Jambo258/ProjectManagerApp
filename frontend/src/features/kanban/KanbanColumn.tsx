1a8be2fa9cc1008a609c100306f0b53d2015b735
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-02-08 11:33:25 +0200

    Fix overflow

--- frontend/src/features/kanban/KanbanColumn.tsx
+++ frontend/src/features/kanban/KanbanColumn.tsx
@@ -100,7 +100,7 @@ export const KanbanColumn = (props: Props) => {
     <div
       ref={setNodeRef}
       style={style}
-      className="bg bg-grayscale-200 w-[300px] h-[500px] max-h-[500px] rounded-md flex flex-col overflow-visible"
+      className="bg bg-grayscale-200 w-[300px] h-[500px] max-h-[500px] rounded-md flex flex-col overflow-y-auto overflow-x-visible"
     >
       <div
         {...attributes}
