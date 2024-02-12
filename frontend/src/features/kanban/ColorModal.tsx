c971c1015c60740f1be5d763bece3cfd7f5f9641
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-02-12 14:52:09 +0200

    Fix wrong hover color

--- frontend/src/features/kanban/ColorModal.tsx
+++ frontend/src/features/kanban/ColorModal.tsx
@@ -14,7 +14,7 @@ export const ColorModal = ({ label, setValue }: ColorProps) => {
   return (
     <button
       onClick={() => handleChange()}
-      className={`h-8 rounded w-full ${label.color}`}
+      className={`h-8 rounded w-full ${label.color} ${label.hover}`}
       type="button"
     />
   );
