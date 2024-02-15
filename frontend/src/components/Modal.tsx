af25cc4c3d206e08fb7ccf6e90776101ebb587b5
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-02-15 11:20:27 +0200

    Modal: remove unnecessary comments

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -67,7 +67,6 @@ export const Modal = ({
         <dialog
           aria-modal="true"
           onClick={(e) => e.stopPropagation()}
-          // The sizing of the modal (w, min-w and max-w) might need to be modified
           className={`fixed p-2 pb-4 flex flex-col inset-0 z-30 max-h-screen sm:justify-start items-left overflow-x-hidden overflow-y-auto outline-none sm:rounded focus:outline-none shadow transition-all
           ${screenDimensions.height < 500 ? "min-h-screen w-full" : "w-full h-full sm:h-fit sm:w-fit sm:max-w-2xl"}`}>
           <header className="w-full flex flex-col mb-2 place-items-end">
